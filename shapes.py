5d3b52b09005a0082d66bfea0c35a7032a00a2f9
Charlotte Patola <charlotte.patola@helsinki.fi>
2019-03-18 16:57:56 +0200

    decimals

--- shapes.py
+++ shapes.py
@@ -14,18 +14,18 @@ def main():
             base = input("Give base of the triangle:")
             height = input("Give height of the triangle:")
             area =0.5*float(base)*float(height)    
-            print("The area is","%6f" %area)
+            print("The area is","%2f" %area)
     
         elif choose in ['rectangle','RECTANGLE','Rectangle']:
             width = input("Give width of the rectangle:")
             height = input("Give height of the rectangle:")
             area =float(width)*float(height)
-            print("The area is","%6f" %area)  
+            print("The area is","%2f" %area)  
     
         elif choose in ['circle','CIRCLE','Circle']:
             radius = input("Give radius of the circle:")
             area =(float(radius)**2)*3.14159265358979323846
-            print("The area is","%6f" %area)
+            print("The area is","%2f" %area)
 
         else:
             print("Unknown shape!")
