c5b0b690a0e65e09099f5466ae54765a6afff820
Charlotte Patola <charlotte.patola@helsinki.fi>
2019-03-19 21:25:29 +0200

    More basic functionality

--- shapes.py
+++ shapes.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-import math
-
-def main():
-    # enter you solution here
+ 
+def shapes():
+    import math
     while True:
         choose = input("Choose a shape (triangle, rectangle, circle). q quits:")
         
@@ -29,6 +28,57 @@ def main():
 
         else:
             print("Unknown shape! Try again!")
+def funny():
+    import random
+    print("Funny shape fact:\n")
+    Funnyinfo =[
+            "The first theorems relating to circles are attributed to Thales around 650 BC",
+            "A triangle is the sign for God, Jesus and The Holy Spirit in christianity",
+            "A rectangle is the most common shape for a rug",
+            "Infants prefere circles to other shapes"]
+    print(random.choice(Funnyinfo))
+        
+    reply =input("Interesting, right?")
+    if input== "ok":
+        ui()
+    else:
+        ui()
+    
 
-if __name__ == "__main__":
-    main()
+def stat():
+    print("No stats so far")
+    
+def ui():
+    while True:
+        ask=input("""\n***** This is a programme for calculating areas of shapes *****\n 
+        What do you want to do? \n
+        Press...\n
+        c to -> Calculates areas for shapes\n
+        s for -> Statistics\n
+        f for -> Funny shape info\n
+        q to quit """)
+    
+        if ask =="q":
+            sure = input("Are you sure you want to quit? y/n")
+            if sure == "y":
+                break
+            else:
+                continue
+        elif ask =="c":
+            shapes()
+        elif ask =="s":
+            stat()
+        elif ask =="f":
+            funny()
+        else:
+            print("Unknown input")
+            continue
+            
+
+                     
+        
+
+def main():
+    ui()
+
+main()
